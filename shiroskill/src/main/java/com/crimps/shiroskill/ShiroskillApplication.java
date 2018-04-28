e1846ba3c8a0bd561466c14d1e0f7015d5594fae
crimps <woolpack.cwz@gmail.com>
2018-04-28 15:51:54 +0800

    spring boot 集成 shiro

---
+++ shiroskill/src/main/java/com/crimps/shiroskill/ShiroskillApplication.java
@@ -0,0 +1,12 @@
+package com.crimps.shiroskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroskillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroskillApplication.class, args);
+    }
+}
---
+++ shiroskill/src/test/java/com/crimps/shiroskill/ShiroskillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.crimps.shiroskill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiroskillApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
