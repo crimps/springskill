3b0a078fb4076d5fe2c9288d051c16d437d7897d
crimps <woolpack.cwz@gmail.com>
2018-06-01 16:19:38 +0800

    shiro

--- shiroskill/src/main/java/com/crimps/shiroskill/service/SysUserService.java
+++ shiroskill/src/main/java/com/crimps/shiroskill/service/SysUserService.java
@@ -5,9 +5,9 @@ import com.crimps.shiroskill.domain.entity.SysUser;
 public interface SysUserService {
 
     /**
-     *
-     * @param userName
-     * @return
+     * 根据用户名获取用户信息
+     * @param userName 用户名
+     * @return 用户信息
      */
     SysUser findByUsername(String userName);
 }
